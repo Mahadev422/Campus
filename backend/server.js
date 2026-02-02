422b8ab973cce594ae1b3d28518e89b320f3ea2b
Mahadev <project9162595119@gmail.com>
2026-02-02 11:24:49 +0530

    fully fixed

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
 import express from "express";
 import { configDotenv } from "dotenv";
-import cors from "cors";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
-import path from 'path';
+import path from "path";
 import { fileURLToPath } from "url";
-// local file
+
 import clubRouter from "./src/routes/club.route.js";
 import authRouter from "./src/routes/auth.route.js";
 import userRouter from "./src/routes/user.route.js";
@@ -20,28 +19,30 @@ app.use(cookieParser());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/club", clubRouter);
 app.use("/api/event", eventRouter);
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get((req, res) => {
-  res.sendFile(
-    path.join(__dirname, "../frontend/dist/index.html")
-  );
+const distPath = path.join(__dirname, "../frontend/dist");
+app.use(express.static(distPath));
+
+
+app.use((req, res) => {
+  res.sendFile(path.join(distPath, "index.html"));
 });
+
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.info("Connected");
 
-    // listening app
-    const PORT = process.env.PORT;
+    const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => {
-      console.log(`Server is runnig  in http://localhost:${PORT}`);
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => {
